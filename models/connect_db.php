2bac523b8b682b361403d51b0cb67da9212e111d
demichuang <demichuang40207@gmail.com>
2016-08-02 23:23:47 +0800

    Update connect_db.php

--- models/connect_db.php
+++ models/connect_db.php
@@ -4,9 +4,9 @@ class connect_db{
     public function __construct()
     {
         $this->db= new PDO('mysql:host=127.0.0.1;dbname=new;charset=utf8',
-                    'root',
-                    ''
-                     );
+                           'root',
+                           ''
+                           );
     }
 }
 ?>
